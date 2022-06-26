44f0a5a733559f5a641ef495eb70c260a60d890b
amit patel <amitv9493@gmail.com>
2022-06-26 11:12:57 +0530

    add css foundation

--- mypy.py
+++ mypy.py
@@ -1,1 +1,14 @@
-import os 
+
+arr = [1, 2, 3, 5]
+
+def insert(arr, index, successor):
+    for index in range(index, len(arr)):
+        arr[index], successor = successor, arr[index]
+
+    arr.append(successor)
+
+print("Array before insertion:", arr)
+# OUTPUT: [1, 2, 3, 5]
+insert(arr, 3, 4) 
+print("Array after insertion:", arr)
+# OUTPUT: [1, 2, 3, 4, 5]
